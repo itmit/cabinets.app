a157ecc2311452af31bcbfa3e8a26cf1b8faaac9
fastandfurios <semu-dima@yandex.ru>
2020-01-09 16:00:11 +0300

    Add project files.

---
+++ cabinets/cabinets/Pages/AuthorizationPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace cabinets.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AuthorizationPage : ContentPage
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
