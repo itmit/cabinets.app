52abc71abbe44133c1cc8faa1fe1e4a64f24f3ff
fastandfurios <semu-dima@yandex.ru>
2020-03-25 16:36:00 +0300

    страница восстановления пароля

---
+++ src/cabinets.Core/Pages/Design/DesignClass.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+
+namespace cabinets.Core.Pages.Design
+{
+	static class DesignClass
+	{
+		public static string FontRegular, FontBold;
+
+		static DesignClass()
+		{
+			if (Device.iOS == Device.RuntimePlatform)
+			{
+				FontRegular = "Lato-Regular";
+				FontBold = "Lato-Bold";
+			}
+			else if(Device.Android == Device.RuntimePlatform)
+			{
+				FontRegular = "Lato-Regular.ttf#Lato-Regular";
+				FontBold = "Lato-Bold.ttf#Lato-Bold";
+			}
+		}
+	}
+}
